a1bade557173660ee7fed6cd42e9ff1db2f87111
realKimJaeJUN <nagandr1113@gmail.com>
2022-11-14 19:51:25 +0900

    팜스토리 프로젝트 예제 추가

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hello PHP</title>
+	</head>
+
+	<body>
+		<h1>Hello PHP!!!</h1>
+		<?php
+			$message = "Hello PHP!";
+			echo "<h3>".$message."</h3>";
+		?>
+	</body>
+</html>
